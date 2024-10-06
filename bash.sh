965f033cc83cdf9c615e94356ba4e9546ce730c1
NazariyPylypchuk <nazariylife@gmail.com>
2024-10-06 21:42:18 +0300

    bash

--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash 
 COUNTER=0 
 while [ $COUNTER -lt 10 ] 
@@ -5,3 +6,12 @@ do
    echo The counter is $COUNTER 
    let COUNTER=$COUNTER+1 
 done!
+=======
+#!/bin/bash 
+COUNTER=0 
+while [ $COUNTER -lt 10 ] 
+do 
+   echo The counter is $COUNTER 
+   let COUNTER=$COUNTER+1 
+done
+>>>>>>> fd549fcd45c0f4a496cdb9be17899a313f7fc3da
